6e624e56b38ef624ac5dc4428772c34dc16383a0
Benjamin Harris <benjamin3harris@gmail.com>
2013-05-29 00:35:05 +0300

    Update underscore, website

--- server/response_commands.js
+++ server/response_commands.js
@@ -8,44 +8,46 @@ galaxy.addListener('message#meteor', function (from, message) {
 		var command = message.match(/\S+/g)[0];
 		switch (command) {
 			case '!np':
-			galaxy.say('#meteor', "No problem! " + from + " was happy to help. If you have any other questions, jump right in and ask!");
-			break;
+				galaxy.say('#meteor', "No problem! " + from + " was happy to help. If you have any other questions, jump right in and ask!");
+				break;
 			case '!mrt':
 			case '!meteorite':
-			if (words.length === 0) {
+				if (words.length === 0) {
 					// Default information
 					galaxy.say('#meteor', "Meteorite is a Meteor version manager and package manager. It provides an easy way to run different versions of Meteor, use non-core packages, and to install packages from the Atmosphere package repository. http://oortcloud.github.io/meteorite/");
 				} else {
 					// Info about a specific command
+					console.log('Failed to document MRT, with "coming soon" notice'); // Debug
 					galaxy.say('#meteor', "That feature is coming soon...");
 				}
 				break;
-				case '!atmo':
-				case '!atmosphere':
-				case '!package':
-				case '!packages':
+			case '!atmo':
+			case '!atmosphere':
+			case '!package':
+			case '!packages':
 				if (words.length === 0) {
 					// Default information
 					galaxy.say('#meteor', "Atmosphere is a community-maintained Meteor smart package repository. It works with Meteorite, a Meteor version and smart package manager. https://atmosphere.meteor.com/");
 				} else {
 					// Info about a specific package
+					console.log('Package docs requested: ' + words[0]); // Debug
 					galaxy.say('#meteor', getPackageInfo(words[0]));
 				}
 				break;
-				case '!docs':
+			case '!docs':
 				galaxy.say('#meteor', "That feature is coming soon...");
 				break;
-				case '!dataja':
+			case '!dataja':
 				galaxy.say('#meteor', "Don't ask to ask, Just ask!");
 				break;
-				case '!ugt':
+			case '!ugt':
 				galaxy.say('#meteor', "It is always morning when someone comes into a channel. We call that Universal Greeting Time http://www.total-knowledge.com/~ilya/mips/ugt.html")
 				break;
-				default:
+			default:
 				galaxy.say('#meteor', invalidCommandResponseList[ _.random(0, invalidCommandResponseList.length - 1) ]);
-			}
 		}
-	});
+	}
+});
 
 var invalidCommandResponseList = [
 	"What's that? A command?",
