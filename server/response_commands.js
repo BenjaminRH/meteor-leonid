0e5e2e2b7fccc10621a89323964a7d035ccbb4d8
Benjamin Harris <benjamin3harris@gmail.com>
2013-05-28 11:38:44 +0300

    Improve commands a bit, update site

--- server/response_commands.js
+++ server/response_commands.js
@@ -4,11 +4,34 @@ galaxy.addListener('message#meteor', function (from, message) {
 
 	if (message[0] === '!') {
 		// It's a command!
-		var words = message.match(/\w+/g);
+		var words = message.match(/\w+/g).slice(1);
 		var command = message.match(/\S+/g)[0];
-		switch (message) {
+		switch (command) {
+			case '!np':
+				galaxy.say('#meteor', "No problem! " + from + " was happy to help. If you have any other questions, jump right in and ask!");
+				break;
+			case '!mrt':
+			case '!meteorite':
+				if (words.length === 0) {
+					// Default information
+					galaxy.say('#meteor', "Meteorite is a Meteor version manager and package manager. It provides an easy way to run different versions of Meteor, use non-core packages, and to install packages from the Atmosphere package repository. http://oortcloud.github.io/meteorite/");
+				} else {
+					// Info about a specific command
+					galaxy.say('#meteor', "That feature is coming soon...");
+				}
+				break;
+			case '!atmo':
+			case '!atmosphere':
+				if (words.length === 0) {
+					// Default information
+					galaxy.say('#meteor', "Atmosphere is a community-maintained Meteor smart package repository. It works with Meteorite, a Meteor version and smart package manager. https://atmosphere.meteor.com/");
+				} else {
+					// Info about a specific package
+					galaxy.say('#meteor', "That feature is coming soon...");
+				}
+				break;
 			case '!docs':
-				galaxy.say('#meteor', 'That feature is coming soon...');
+				galaxy.say('#meteor', "That feature is coming soon...");
 				break;
 			case '!dataja':
 				galaxy.say('#meteor', "Don't ask to ask, Just ask!");
